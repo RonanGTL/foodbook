95290fe39ca940846f67299d60d5f4ce72abb0ed
Heeks <seoheekwon@gmail.com>
2015-01-07 18:01:54 +0000

    author information shown on the post and comment

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,6 @@
 class FoodsController < ApplicationController
+	before_action :login_check
+	skip_before_action :login_check, :only => [:posts, :posts_category, :show]
   def posts
 		@posts= Post.all
   end
@@ -19,6 +21,7 @@ class FoodsController < ApplicationController
 
   def show
 		@post=Post.find(params[:id])
+		@comment_writer= User.where(id: session[:user_id])[0]
   end
 
   def write
@@ -26,6 +29,7 @@ class FoodsController < ApplicationController
 
   def write_complete
 		post=Post.new
+		post.user_id=session[:user_id]
 		post.category=params[:post_category]
 		post.title=params[:post_title]
 		post.content= params[:post_content]
@@ -65,6 +69,7 @@ class FoodsController < ApplicationController
 	
 	def write_comment_complete
 		comment = Comment.new
+		comment.user_id= session[:user_id]
 		comment.post_id= params[:post_id]
 		comment.content= params[:comment_content]
 		comment.save
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-categories=["Asian","American","European","International"]
+u= User.new
+u.username="charles"
+u.password="12345678"
+u.save
 
-categories.each do |category|
-	0.upto(1) do |i|
-		p= Post.new
-		p.user_id= i+1
-		p.category= category
-		p.title = "#{category} restaurant#{i} name"
-		p.content= "#{category} restaurant#{i} content"
-		p.save
-	end
-end
+u= User.new
+u.username="robert"
+u.password="87654321"
+u.save
