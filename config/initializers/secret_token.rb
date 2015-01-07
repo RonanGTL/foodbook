5cac2baf384179620d4bda9414b015c37f06f304
Heeks <seoheekwon@gmail.com>
2015-01-07 10:24:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodbook::Application.config.secret_key_base = '276bc6920beceba87e8682c56d87bf45204cd555ecc3c5398e379ce7474954912f99db219fed181278ef5697a833d5f9ace4c487d83c6e5349db5fbe285fc982'
