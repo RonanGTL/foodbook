531697f4125f569baffc289d9c6409f1a73b7dd8
Heeks <seoheekwon@gmail.com>
2015-01-07 14:54:27 +0000

    post deletion implemented

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -57,5 +57,9 @@ class FoodsController < ApplicationController
   end
 
   def delete_complete
+		post=Post.find(params[:id])
+		post.destroy
+		flash[:alert] = "Deleted successfully."
+		redirect_to "/"
   end
 end
